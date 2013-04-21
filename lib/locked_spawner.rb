95878c4fd7175a21e9174232d82d8e6f5da9180b
Michael Whittaker <post@michael-whittaker.de>
2013-04-21 21:54:21 +0200

    removes unncecessary redis.reconnect

--- lib/locked_spawner.rb
+++ lib/locked_spawner.rb
@@ -5,7 +5,6 @@ class LockedSpawner
     else
       Spawner.spawn do
         redis = Redis.new(::REDIS_URL) if defined?(::REDIS)
-        redis.reconnect if defined?(redis)
         redis.set "crawling", 1 if defined?(redis)
         ZSBCrawler.new.fetch_all true
         redis.set "crawling", 0 if defined?(redis)
