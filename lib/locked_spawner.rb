df1705fc31d628acac7ea87c2fba2ca1b9b40ba5
Michael Whittaker <post@michael-whittaker.de>
2013-04-21 21:23:12 +0200

    if bug fix and no admin http caching

--- lib/locked_spawner.rb
+++ lib/locked_spawner.rb
@@ -1,6 +1,6 @@
 class LockedSpawner
   def self.crawl
-    if defined?(::REDIS) && ::REDIS.get("crawling") != 1
+    if defined?(::REDIS) && ::REDIS.get("crawling") == 1
       return false
     else
       Spawner.spawn do
